d359f0cfb34f148c2e8d59b4f6cb98a05c42b39c
James <jamiemackenzie57@gmail.com>
2024-11-15 13:55:23 +0100

    Feat/opg scanning poc (#6)
    * feat/opg-scanning: initial commit of base structure
    
    * feat/opg-scanning-poc: simplification of factory parser format type condition
    
    * feat/opg-scanning-poc: refactored approach after deeper analysis of ddc php compoenents.
    
    * feat/opg-scanning-poc: integrate centralized logging in ingestion queue.
    
    * feat/opg-scanning-poc: job queue test implementation - added proper processed jobs completed test with embedded callback.
    
    * feat/opg-scanning-poc: added empty callback to avoid breaking changes..
    
    * feat/opg-scanning-poc: fixed typo in readme.
    
    * feat/opg-scanning-poc: added context-based managment to worker pool to improve robustness
    
    * feat/opg-scanning-poc: implemented xsd validation / cleaned redundant code and refactoring.
    
    * feat/opg-scanning-poc: bug fixes and prep for test handler of api.
    
    * feat/opg-scanning-poc: fixed config.go implementation, improved ingestion, and slight refactor / clean-up of handler.go.
    
    * feat/opg-scanning-poc: first handler test pass for xsd validation.
    
    * feat/opg-scanning-poc: updated handler_test.go.
    
    * feat/opg-scanning-poc: better logging and improved config.go / added support for both .env and yaml.
    
    * feat/opg-scanning-poc: removed log dir.
    
    * feat/opg-scanning-poc: added parser vaidators based on struct required comments / clean-up.
    
    * Fix build pipeline
    
    - Configure CodeQL for Go
    - Add Dockerfile
    - Run tests in pipeline
    
    * Set PROJECT_PATH when running tests
    
    * feat/opg-scanning-poc: test fix for uncontrolled data used in path expression / refactor logger to use OPG.
    
    * feat/opg-scanning-poc: fixed a bug with logger not initalizing in job queue and update Dockerfile to ensure xsd docs are present.
    
    * Fix Trivy and ECR workflow
    
    Log in to ECR, then pull Trivy repositories from it. Use appropriate name for application ECR repository.
    
    * Install AWS CLI before configuring ECR
    
    * Assume correct role
    
    * feat/opg-scanning-poc/types-refactor: initial commit of complete lpf1 type structs / improved santizer and validator to be recursive.
    
    * feat/opg-scanning-poc: refactored factory to be more extensible and modular
    
    * feat/opg-scanning-poc: date validators added / document factory  test
    
    * feat/opg-scanning-poc: clean-up.
    
    * feat/opg-scanning-poc: added alternative xml for test case and extended job_queue_test.go to load multiple xml files directly.
    
    * feat/opg-scanning-poc: added common validators.
    
    * feat/opg-scanning-poc: bug fixes along with common validators implementation and test coverage for lp1f validator
    
    * feat/opg-scanning-poc: fixed issue with validation and introduced handlers for multi page instances
    
    * feat/opg-scanning-poc: fixed bug with array structures being returned for specific fields.
    
    * feat/opg-scanning-poc: better coverage for invalid xml example.
    
    * feat/opg-scanning-poc: fixes against comments in PR - config.yml / README.md removal, dateparser updated, and re-organisation
    
    * feat/opg-scanning-poc: added todo to mark docs when validation fails / logger updated.
    
    * feat/opg-scanning-poc: lp1f validation changes for parseDate
    
    * feat/opg-scanning-poc: added client.go and API handler changes to handle case stub creation.
    
    * feat/opg-scanning-poc: clean-up comments and provided minor fixes based on PR comments.
    
    * fix UID field name
    
    * Remove terraform from workflow
    
    This service doesn't run its own terraform
    
    * Fix timeout error message
    
    * Set default values on config
    
    So the tests can be run without needing environment variables to be set
    
    * Finish removing terraform from workflow
    
    I missed some bits
    
    ---------
    
    Co-authored-by: James Mackenzie <james.mackenzie@digital.justice.gov.uk>
    Co-authored-by: Greg Tyler <greg.tyler@digital.justice.gov.uk>

---
+++ service-app/internal/logger/logger.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"fmt"
+
+	"log/slog"
+
+	"github.com/ministryofjustice/opg-go-common/telemetry"
+	"github.com/ministryofjustice/opg-scanning/config"
+)
+
+type Logger struct {
+	cfg        *config.Config
+	SlogLogger *slog.Logger
+}
+
+func NewLogger() *Logger {
+	slogLogger := telemetry.NewLogger("opg-data-lpa-store/getlist")
+	return &Logger{
+		cfg:        config.NewConfig(),
+		SlogLogger: slogLogger,
+	}
+}
+
+func (l *Logger) Info(message string, args ...interface{}) {
+	logMessage := fmt.Sprintf(message, args...)
+	l.SlogLogger.Info(logMessage)
+}
+
+func (l *Logger) Error(message string, args ...interface{}) {
+	logMessage := fmt.Sprintf(message, args...)
+	l.SlogLogger.Error(logMessage)
+}
