428ae5db43c6228f1235812687f97b972d4333bf
James <jamiemackenzie57@gmail.com>
2024-11-25 13:24:32 +0000

    feat/SSM-11: added client_test.go and mock api for testing required r… (#16)
    * feat/SSM-11: added client_test.go and mock api for testing required responses from stub case creation.
    
    * feat/SSM-11-handle-stub-cases: improved test case preformance and expanded coverage.
    
    * feat/SSM-11-handle-stub-cases: todo comment added for cases that are unhandled.
    
    * feat/SSM-11-handle-stub-cases: clean-up and slight refactor.
    
    * feat/SSM-11-handle-stub-cases: further adaptations after interrogating sirius api.
    
    * feat/SSM-11-handle-stub-cases: docker cleanup
    
    * feat/SSM-11-handle-stub-cases: test logic clean-up
    
    * feat/SSM-11-handle-stub-cases: adaptations to fix build due to ongoing api changes.
    
    * feat/SSM-11-handle-stub-cases: removed required attribute in struct since default value is presented.
    
    * feat/SSM-23-auth-middleware: initial commit without full test coverage of working API interaction with Sirius.
    
    * feat/SSM-23-auth-middleware: Clean-up
    
    * feat/SSM-23-auth-middleware: docker file dev network commented
    
    * feat/SSM-23-auth-middleware: removed token expiry as its not longer needed
    
    * feat/SSM-23-auth-middleware: SSM-11 fixes based on latest commnets.
    
    * feat/SSM-23-auth-middleware: extended document type constants
    
    * feat/SSM-11-handle-stub-cases: removed basic auth layer pending jwt implementation.
    
    * feat/SSM-11-handle-stub-cases: reverted introduction of .env
    
    * feat/SSM-11-handle-stub-cases: reverted introduction of .env
    
    * feat/SSM-11-handle-stub-cases: removed networks
    
    * feat/SSM-11-handle-stub-cases: fix for constants and timestamp.
    
    * feat/SSM-11-handle-stub-cases: set validation updated with instrument correction.
    
    ---------
    
    Co-authored-by: James Mackenzie <james.mackenzie@digital.justice.gov.uk>

---
+++ service-app/internal/constants/document_types.go
@@ -0,0 +1,66 @@
+package constants
+
+const (
+	DocumentTypeLPA002  = "LPA002"
+	DocumentTypeLP0002R = "LPA002R"
+	DocumentTypeLPAPA   = "LPA-PA"
+	DocumentTypeLPAPW   = "LPA-PW"
+	DocumentTypeLPA114  = "LPA114"
+	DocumentTypeLPA117  = "LPA117"
+	DocumentTypeLP1F    = "LP1F"
+	DocumentTypeLP1H    = "LP1H"
+	DocumentTypeLP2     = "LP2"
+)
+
+const (
+	DocumentTypeEP2PG = "EP2PG"
+	DocumentTypeEPA   = "EPA"
+)
+
+const (
+	DocumentTypeCOPORD = "COPORD"
+)
+
+var (
+	LPATypeDocuments = []string{
+		DocumentTypeLPA002,
+		DocumentTypeLP1F,
+		DocumentTypeLP1H,
+		DocumentTypeLP2,
+	}
+
+	EPATypeDocuments = []string{
+		DocumentTypeEP2PG,
+		DocumentTypeEPA,
+	}
+
+	Instruments = []string{
+		DocumentTypeEPA,
+		DocumentTypeLPAPW,
+		DocumentTypeLPAPA,
+		DocumentTypeLPA114,
+		DocumentTypeLPA117,
+		DocumentTypeLP1F,
+		DocumentTypeLP1H,
+	}
+
+	Applications = []string{
+		DocumentTypeEP2PG,
+		DocumentTypeLPA002,
+		DocumentTypeLP0002R,
+		DocumentTypeLP2,
+	}
+
+	CourtOrderDocuments = []string{
+		DocumentTypeCOPORD,
+	}
+
+	StandaloneInstruments = []string{
+		DocumentTypeLP1F,
+		DocumentTypeLP1H,
+	}
+
+	ExemptApplications = []string{
+		DocumentTypeLP0002R,
+	}
+)
