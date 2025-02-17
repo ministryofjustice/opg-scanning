3d7cdcf9be41936e9f11bfcc052d5ffa051f66bc
James Mackenzie <jamiemackenzie57@gmail.com>
2025-02-17 13:12:41 +0000

    feat/SSM-27: fixed test failures

--- service-app/internal/parser/corresp_parser/corresp_parser.go
+++ service-app/internal/parser/corresp_parser/corresp_parser.go
@@ -1,26 +1,11 @@
 package corresp_parser
 
 import (
-	"encoding/xml"
-	"fmt"
-
 	"github.com/ministryofjustice/opg-scanning/internal/parser"
 	"github.com/ministryofjustice/opg-scanning/internal/types/corresp_types"
 )
 
 func Parse(data []byte) (interface{}, error) {
-	if len(data) == 0 {
-		return nil, fmt.Errorf("correspondence data is empty")
-	}
-
 	doc := &corresp_types.Correspondence{}
-	if err := xml.Unmarshal(data, doc); err != nil {
-		return nil, err
-	}
-	// Validate required fields based on struct tags
-	if err := parser.ValidateStruct(doc); err != nil {
-		return nil, err
-	}
-
-	return doc, nil
+	return parser.DocumentParser(data, doc)
 }
