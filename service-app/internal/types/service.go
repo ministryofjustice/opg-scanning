b2ff53b27fb95012a477497ae7c8ebf0d630025c
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2025-03-03 13:37:54 +0000

    Add Pact tests
    Rather than mocking the HTTP component, use a Pact mock server and generate a contract file.
    
    Upload that file to the broker to ensure the contract can be met by Sirius (the provider).
    
    Fixes SSM-89 #patch

--- service-app/internal/types/service.go
+++ service-app/internal/types/service.go
@@ -21,13 +21,7 @@ type ScannedDocumentRequest struct {
 }
 
 type ScannedDocumentResponse struct {
-	ID                  int    `json:"id"`
 	UUID                string `json:"uuid"`
-	Type                string `json:"type"`
-	FriendlyDescription string `json:"friendlyDescription"`
-	Title               string `json:"title"`
-	SourceDocumentType  string `json:"sourceDocumentType"`
-	Subtype             string `json:"subtype"`
 }
 
 // For handling error responses according to the OpenAPI spec
