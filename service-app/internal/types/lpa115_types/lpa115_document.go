86d55eed22ae8f7722f9bd0869e5393dbd7ce858
James Mackenzie <jamiemackenzie57@gmail.com>
2025-03-06 09:05:38 +0000

    feat/SSM-53: validation added, invalid / valid xml docs added, tests added.

--- service-app/internal/types/lpa115_types/lpa115_document.go
+++ service-app/internal/types/lpa115_types/lpa115_document.go
@@ -8,27 +8,38 @@ import (
 )
 
 type LPA115Document struct {
-	XMLName  xml.Name `xml:"LPA115"`
-	Page1    Page     `xml:"Page1"`
-	PartA    PartA    `xml:"PartA"`
-	Attorney Attorney `xml:"Attorney"`
+	XMLName xml.Name `xml:"LPA115"`
+	Page3   []Page3  `xml:"Page3"`
+	Page6   []Page6  `xml:"Page6"`
 }
 
-type Page struct {
+type Page3 struct {
+	PartA             PartA3 `xml:"PartA"`
+	Signature         bool   `xml:"Signature"`
+	Date              string `xml:"Date"`
+	ContinuationSheet int    `xml:"ContinuationSheetNo"`
+	TotalSheets       string `xml:"TotalSheets"`
 	types.BasePage
 }
 
-type PartA struct {
-	FullName      string             `xml:"FullName"`
-	Address       lp1f_types.Address `xml:"Address"`
-	CaseReference string             `xml:"CaseReference"`
-	Declaration   string             `xml:"Declaration"`
+type PartA3 struct {
+	FullName string                 `xml:"FullName"`
+	OptionA  lp1f_types.Declaration `xml:"OptionA"`
+	OptionB  lp1f_types.Declaration `xml:"OptionB"`
 }
 
-type Attorney struct {
-	FirstName string `xml:"FirstName"`
-	LastName  string `xml:"LastName"`
-	Address   lp1f_types.Address
-	Telephone string `xml:"Telephone"`
-	Email     string `xml:"Email"`
+type Page6 struct {
+	PartB PartBPage6 `xml:"PartB"`
+	types.BasePage
+}
+
+type PartBPage6 struct {
+	Salutation        lp1f_types.Salutation `xml:"Salutation"`
+	FirstName         string                `xml:"FirstName"`
+	LastName          string                `xml:"LastName"`
+	Address           lp1f_types.Address    `xml:"Address"`
+	Signature         bool                  `xml:"Signature"`
+	Date              string                `xml:"Date"`
+	ContinuationSheet int                   `xml:"ContinuationSheetNo"`
+	TotalSheets       int                   `xml:"TotalSheets"`
 }
