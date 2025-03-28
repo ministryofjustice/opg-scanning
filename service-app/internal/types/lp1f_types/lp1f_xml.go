174913e447983d95218292a070e300ae4c066db2
James Mackenzie <jamiemackenzie57@gmail.com>
2025-03-28 07:46:12 +0000

    reverted type go structs for lp1f

--- service-app/internal/types/lp1f_types/lp1f_xml.go
+++ service-app/internal/types/lp1f_types/lp1f_xml.go
@@ -1,10 +1,11 @@
 package lp1f_types
 
-// Address always outputs all four sub-elements.
+import "github.com/ministryofjustice/opg-scanning/internal/types"
+
 type Address struct {
 	Address1 string `xml:"Address1"`
-	Address2 string `xml:"Address2"`
-	Address3 string `xml:"Address3"`
+	Address2 string `xml:"Address2,omitempty"`
+	Address3 string `xml:"Address3,omitempty"`
 	Postcode string `xml:"Postcode"`
 }
 
@@ -14,20 +15,19 @@ type Salutation struct {
 	Ms        bool   `xml:"Ms"`
 	Miss      bool   `xml:"Miss"`
 	Other     bool   `xml:"Other"`
-	OtherName string `xml:"OtherName"`
+	OtherName string `xml:"OtherName,omitempty"`
 }
 
 type PersonName struct {
 	Salutation     Salutation `xml:"Salutation"`
 	LastName       string     `xml:"LastName"`
 	Forename       string     `xml:"Forename"`
-	OtherForenames string     `xml:"OtherForenames"`
+	OtherForenames string     `xml:"OtherForenames,omitempty"`
 }
 
-// Declaration: remove omitempty so Signature and Date always appear.
 type Declaration struct {
-	Signature bool   `xml:"Signed"`
-	Date      string `xml:"Date"`
+	Signature bool   `xml:"Signature,omitempty"`
+	Date      string `xml:"Date,omitempty"`
 }
 
 type Notification struct {
@@ -47,10 +47,10 @@ type Attorney struct {
 	Title            string  `xml:"Title"`
 	FirstName        string  `xml:"FirstName"`
 	LastName         string  `xml:"LastName"`
-	DOB              string  `xml:"DOB"`
-	Address          Address `xml:"Address"`
-	EmailAddress     string  `xml:"EmailAddress"`
-	TrustCorporation bool    `xml:"TrustCorporation"` // not a pointer so it's always output
+	DOB              string  `xml:"DOB,omitempty"`
+	Address          Address `xml:"Address,omitempty"`
+	EmailAddress     string  `xml:"EmailAddress,omitempty"`
+	TrustCorporation *bool   `xml:"TrustCorporation,omitempty"`
 	Declaration
 }
 
@@ -67,7 +67,7 @@ type SkillCertification struct {
 	SocialWorker               bool   `xml:"SocialWorker"`
 	IMCA                       bool   `xml:"IMCA"`
 	NoneOfTheAbove             bool   `xml:"NoneOfTheAbove"`
-	SkillsAndExpertise         string `xml:"SkillsAndExpertise"`
+	SkillsAndExpertise         string `xml:"SkillsAndExpertise,omitempty"`
 }
 
 type Witness struct {
@@ -85,22 +85,15 @@ type Section1 struct {
 	Title        string  `xml:"Title"`
 	FirstName    string  `xml:"FirstName"`
 	LastName     string  `xml:"LastName"`
-	OtherNames   string  `xml:"OtherNames"`
+	OtherNames   string  `xml:"OtherNames,omitempty"`
 	DOB          string  `xml:"DOB"`
 	Address      Address `xml:"Address"`
 	EmailAddress string  `xml:"EmailAddress"`
 }
 
-// Section2 for Page2: uses Attorney1 and Attorney2.
-type Section2Page2 struct {
+type Section2 struct {
 	Attorney1 Attorney `xml:"Attorney1"`
 	Attorney2 Attorney `xml:"Attorney2"`
-}
-
-// Section2 for Page3: two Attorney elements and MoreAttorneys.
-type Section2Page3 struct {
-	Attorneys     []Attorney `xml:"Attorney"` // must contain exactly two elements
-	MoreAttorneys bool       `xml:"MoreAttorneys"`
 }
 
 type Section3 struct {
@@ -112,7 +105,7 @@ type Section3 struct {
 
 type Section4 struct {
 	Attorney1             Attorney `xml:"Attorney1"`
-	Attorney2             Attorney `xml:"Attorney2"`
+	Attorney2             Attorney `xml:"Attorney2,omitempty"`
 	MoreReplacements      bool     `xml:"MoreReplacements"`
 	ChangeHowAttorneysAct bool     `xml:"ChangeHowAttorneysAct"`
 }
@@ -182,119 +175,118 @@ type Section14 struct {
 	ReducedApplicationFee bool   `xml:"ReducedApplicationFee"`
 	RepeatApplication     bool   `xml:"RepeatApplication"`
 	CaseNumber            string `xml:"CaseNumber"`
-	OnlineLPA             bool   `xml:"OnlineLPA"`
-	OnlineLPAID           string `xml:"OnlineLPAID"`
+	OnlineLPA             bool   `xml:"OnlineLPA,omitempty"`
+	OnlineLPAID           string `xml:"OnlineLPAID,omitempty"`
 }
 
 type Section15 struct {
 	Applicant []Declaration `xml:"Applicant"`
 }
 
-// Page structs (each enforces the order: Section, then BURN, then PhysicalPage)
-
 type Page1 struct {
-	Section1     Section1 `xml:"Section1"`
-	BURN         string   `xml:"BURN"`
-	PhysicalPage int      `xml:"PhysicalPage"`
+	types.BasePage
+	Section1 Section1 `xml:"Section1"`
 }
 
 type Page2 struct {
-	Section2     Section2Page2 `xml:"Section2"`
-	BURN         string        `xml:"BURN"`
-	PhysicalPage int           `xml:"PhysicalPage"`
+	types.BasePage
+	Section2 Section2 `xml:"Section2"`
 }
 
 type Page3 struct {
-	Section2     Section2Page3 `xml:"Section2"`
-	BURN         string        `xml:"BURN"`
-	PhysicalPage int           `xml:"PhysicalPage"`
+	types.BasePage
+	Section2 Section3 `xml:"Section3"`
 }
 
 type Page4 struct {
-	Section3     Section3 `xml:"Section3"`
-	BURN         string   `xml:"BURN"`
-	PhysicalPage int      `xml:"PhysicalPage"`
+	types.BasePage
+	Section3 Section3 `xml:"Section3"`
 }
 
 type Page5 struct {
-	Section4     Section4 `xml:"Section4"`
-	BURN         string   `xml:"BURN"`
-	PhysicalPage int      `xml:"PhysicalPage"`
+	types.BasePage
+	Section4 Section4 `xml:"Section4"`
 }
 
 type Page6 struct {
-	Section5     Section5 `xml:"Section5"`
-	BURN         string   `xml:"BURN"`
-	PhysicalPage int      `xml:"PhysicalPage"`
+	types.BasePage
+	Section5 Section5 `xml:"Section5"`
 }
 
 type Page7 struct {
-	Section6     Section6 `xml:"Section6"`
-	BURN         string   `xml:"BURN"`
-	PhysicalPage int      `xml:"PhysicalPage"`
+	types.BasePage
+	Section6 Section6 `xml:"Section6"`
 }
 
 type Page8 struct {
-	Section7     Section7 `xml:"Section7"`
-	BURN         string   `xml:"BURN"`
-	PhysicalPage int      `xml:"PhysicalPage"`
+	types.BasePage
+	Section7 Section7 `xml:"Section7"`
 }
 
 type Page9 struct {
-	Section8     string `xml:"Section8"`
-	BURN         string `xml:"BURN"`
-	PhysicalPage int    `xml:"PhysicalPage"`
+	types.BasePage
+	Section8 string `xml:"Section8"`
 }
 
 type Page10 struct {
-	Section9     Section9 `xml:"Section9"`
-	BURN         string   `xml:"BURN"`
-	PhysicalPage int      `xml:"PhysicalPage"`
+	types.BasePage
+	Section9 Section9 `xml:"Section9"`
 }
 
 type Page11 struct {
-	Section10    Section10 `xml:"Section10"`
-	BURN         string    `xml:"BURN"`
-	PhysicalPage int       `xml:"PhysicalPage"`
+	types.BasePage
+	Section10 Section10 `xml:"Section10"`
 }
 
 type Page12 struct {
-	Section11    Section11 `xml:"Section11"`
-	BURN         string    `xml:"BURN"`
-	PhysicalPage int       `xml:"PhysicalPage"`
+	types.BasePage
+	Section11 Section11 `xml:"Section11"`
 }
 
 type Page16 struct {
-	Section16    string `xml:"Section16"`
-	BURN         string `xml:"BURN"`
-	PhysicalPage int    `xml:"PhysicalPage"`
+	types.BasePage
+	Section16 string `xml:"Section16"`
 }
 
 type Page17 struct {
-	Section12    Section12 `xml:"Section12"`
-	BURN         string    `xml:"BURN"`
-	PhysicalPage int       `xml:"PhysicalPage"`
+	types.BasePage
+	Section12 Section12 `xml:"Section12"`
 }
 
 type Page18 struct {
-	Section13    Section13 `xml:"Section13"`
-	BURN         string    `xml:"BURN"`
-	PhysicalPage int       `xml:"PhysicalPage"`
+	types.BasePage
+	Section13 Section13 `xml:"Section13"`
 }
 
 type Page19 struct {
-	Section14    Section14 `xml:"Section14"`
-	BURN         string    `xml:"BURN"`
-	PhysicalPage int       `xml:"PhysicalPage"`
+	types.BasePage
+	Section14 Section14 `xml:"Section14"`
 }
 
 type Page20 struct {
-	Section15    Section15 `xml:"Section15"`
-	BURN         string    `xml:"BURN"`
-	PhysicalPage int       `xml:"PhysicalPage"`
+	types.BasePage
+	Section15 Section15 `xml:"Section15"`
 }
 
-// Continuation pages and sheets
+type ContinuationPage1 struct {
+	types.BasePage
+	ContinuationSheet1 ContinuationSheet1 `xml:"ContinuationSheet1"`
+}
+
+type ContinuationPage2 struct {
+	types.BasePage
+	ContinuationSheet2 ContinuationSheet2 `xml:"ContinuationSheet2"`
+}
+
+type ContinuationPage3 struct {
+	types.BasePage
+	ContinuationSheet3 ContinuationSheet3 `xml:"ContinuationSheet3"`
+}
+
+type ContinuationPage4 struct {
+	types.BasePage
+	ContinuationSheet4 ContinuationSheet4 `xml:"ContinuationSheet4"`
+}
 
 type ContinuationSheet1 struct {
 	Attorney []Attorney  `xml:"Attorney"`
@@ -317,38 +309,13 @@ type ContinuationSheet4 struct {
 	AuthorisedPerson    []AuthorisedPerson `xml:"AuthorisedPerson"`
 }
 
-type ContinuationPage1 struct {
-	ContinuationSheet1 ContinuationSheet1 `xml:"ContinuationSheet1"`
-	BURN               string             `xml:"BURN"`
-	PhysicalPage       int                `xml:"PhysicalPage"`
-}
-
-type ContinuationPage2 struct {
-	ContinuationSheet2 ContinuationSheet2 `xml:"ContinuationSheet2"`
-	BURN               string             `xml:"BURN"`
-	PhysicalPage       int                `xml:"PhysicalPage"`
-}
-
-type ContinuationPage3 struct {
-	ContinuationSheet3 ContinuationSheet3 `xml:"ContinuationSheet3"`
-	BURN               string             `xml:"BURN"`
-	PhysicalPage       int                `xml:"PhysicalPage"`
-}
-
-type ContinuationPage4 struct {
-	ContinuationSheet4 ContinuationSheet4 `xml:"ContinuationSheet4"`
-	BURN               string             `xml:"BURN"`
-	PhysicalPage       int                `xml:"PhysicalPage"`
-}
-
 type AuthorisedPerson struct {
 	FullName string `xml:"FullName"`
 	Declaration
 }
 
 type InfoPage struct {
-	BURN         string `xml:"BURN"`
-	PhysicalPage int    `xml:"PhysicalPage"`
+	types.BasePage
 }
 
 type AdditionalInformation struct {
