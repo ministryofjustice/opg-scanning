30afd23e01581e8b44b4c7e465450fa7cf53289f
James Mackenzie <jamiemackenzie57@gmail.com>
2025-04-03 11:53:10 +0100

    feat/SSM-134: removed wait and errChan handler.

--- service-app/internal/ingestion/job_queue.go
+++ service-app/internal/ingestion/job_queue.go
@@ -46,21 +46,19 @@ func NewJobContext(reqCtx context.Context) context.Context {
 	return logger.ContextWithLogger(ctx, enrichedLogger)
 }
 
-func (q *JobQueue) AddToQueueSequentially(ctx context.Context, cfg *config.Config, data *types.BaseDocument, format string, onComplete func(ctx context.Context, processedDoc interface{}, originalDoc *types.BaseDocument) error) {
+func (q *JobQueue) AddToQueueSequentially(ctx context.Context, cfg *config.Config, data *types.BaseDocument, format string, onComplete func(ctx context.Context, processedDoc interface{}, originalDoc *types.BaseDocument) error) error {
 	// Create a job context
 	jobCtx := NewJobContext(ctx)
 
 	// Initialize the registry and processor synchronously
 	registry, err := factory.NewRegistry()
 	if err != nil {
-		q.recordError(fmt.Errorf("failed to create registry: %v", err))
-		return
+		return fmt.Errorf("failed to create registry: %v", err)
 	}
 
 	processor, err := factory.NewDocumentProcessor(data, data.Type, format, registry, q.logger)
 	if err != nil {
-		q.recordError(fmt.Errorf("failed to initialize processor: %v", err))
-		return
+		return fmt.Errorf("failed to initialize processor: %v", err)
 	}
 
 	// Use a per-job timeout context.
@@ -69,15 +67,16 @@ func (q *JobQueue) AddToQueueSequentially(ctx context.Context, cfg *config.Confi
 
 	parsedDoc, err := processor.Process(processCtx)
 	if err != nil {
-		q.recordError(fmt.Errorf("failed to process job: %v", err))
-		return
+		return fmt.Errorf("failed to process job: %v", err)
 	}
 
 	if onComplete != nil {
 		if err = onComplete(processCtx, parsedDoc, data); err != nil {
-			q.recordError(fmt.Errorf("onComplete error: %v", err))
+			return fmt.Errorf("onComplete error: %w", err)
 		}
 	}
+
+	return nil
 }
 
 func (q *JobQueue) AddToQueue(ctx context.Context, cfg *config.Config, data *types.BaseDocument, format string, onComplete func(ctx context.Context, processedDoc interface{}, originalDoc *types.BaseDocument) error) {
