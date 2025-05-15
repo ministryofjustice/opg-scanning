2d8ea6f2e50c3612a6ae9c0bb3949790a1522582
Joshua Hawxwell <m@hawx.me>
2025-05-15 12:03:34 +0100

    Refactor httpclient package to a sirius package

--- service-app/internal/api/queue_handler.go
+++ service-app/internal/api/queue_handler.go
@@ -20,7 +20,7 @@ func (c *IndexController) processQueue(ctx context.Context, scannedCaseResponse 
 		// Here we use AddToQueueSequentially to ensure that the documents are processed in order.
 		err := c.Queue.AddToQueueSequentially(ctx, c.config, doc, "xml", func(ctx context.Context, processedDoc any, originalDoc *types.BaseDocument) error {
 			// Create a new service instance for attaching documents.
-			service := newService(newClient(c.httpMiddleware), parsedBaseXml)
+			service := newService(c.siriusClient, parsedBaseXml)
 			service.originalDoc = originalDoc
 
 			attchResp, decodedXML, docErr := service.AttachDocuments(ctx, scannedCaseResponse)
