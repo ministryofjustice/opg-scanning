150c1a6c5f1362f2f4a4f75fac5abfb79fb4a7f7
Joshua Hawxwell <m@hawx.me>
2025-06-02 09:32:23 +0100

    Refactor httpclient package to a sirius package (#136)

--- service-app/internal/api/processor.go
+++ service-app/internal/api/processor.go
@@ -4,30 +4,31 @@ import (
 	"time"
 
 	"github.com/ministryofjustice/opg-scanning/internal/constants"
+	"github.com/ministryofjustice/opg-scanning/internal/sirius"
 	"github.com/ministryofjustice/opg-scanning/internal/types"
 	"github.com/ministryofjustice/opg-scanning/internal/util"
 )
 
-func determineCaseRequest(set *types.BaseSet) (*types.ScannedCaseRequest, error) {
+func determineCaseRequest(set *types.BaseSet) (*sirius.ScannedCaseRequest, error) {
 	now := time.Now().Format(time.RFC3339)
 
 	for _, doc := range set.Body.Documents {
 		if util.Contains(constants.CreateLPADocuments, doc.Type) {
-			return &types.ScannedCaseRequest{
+			return &sirius.ScannedCaseRequest{
 				BatchID:     set.Header.Schedule,
 				CaseType:    "lpa",
 				ReceiptDate: formatScannedDate(set.Header.ScanTime),
 				CreatedDate: now,
 			}, nil
 		} else if util.Contains(constants.CreateEPADocuments, doc.Type) {
-			return &types.ScannedCaseRequest{
+			return &sirius.ScannedCaseRequest{
 				BatchID:     set.Header.Schedule,
 				CaseType:    "epa",
 				ReceiptDate: formatScannedDate(set.Header.ScanTime),
 				CreatedDate: now,
 			}, nil
 		} else if doc.Type == constants.DocumentTypeCOPORD && set.Header.CaseNo != "" {
-			return &types.ScannedCaseRequest{
+			return &sirius.ScannedCaseRequest{
 				CourtReference: set.Header.CaseNo,
 				BatchID:        set.Header.Schedule,
 				CaseType:       "order",
