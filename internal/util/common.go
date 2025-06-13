c20e71e653d629d766e2f46d81a8559fd57df406
Joshua Hawxwell <m@hawx.me>
2025-06-13 10:35:24 +0100

    Move code out of ./service-app

--- internal/util/common.go
+++ internal/util/common.go
@@ -17,7 +17,7 @@ func GetProjectRoot() (string, error) {
 
 	// Adjust the path to go to the project root. This depends on where this file is located.
 	// If `common.go` is in `internal/util`, we need to go up 3 directories to reach the project root.
-	projectRoot := filepath.Join(basePath, "../../..")
+	projectRoot := filepath.Join(basePath, "../..")
 	absPath, err := filepath.Abs(projectRoot)
 	if err != nil {
 		return "", fmt.Errorf("failed to get absolute path: %w", err)
