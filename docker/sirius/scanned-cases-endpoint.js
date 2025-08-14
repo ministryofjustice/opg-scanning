72e4c980dce2fb79c7d868e94f513298d09ac513
Joshua Hawxwell <m@hawx.me>
2025-08-14 09:34:37 +0100

    Add some more integration level tests for unhappy paths (#161)

--- docker/sirius/scanned-cases-endpoint.js
+++ docker/sirius/scanned-cases-endpoint.js
@@ -1,11 +1,19 @@
-const { respond, stores } = require("@imposter-js/types");
+const { respond, stores, context } = require("@imposter-js/types");
 
 const lpaIdStore = stores.open("lpaIdStore");
 
-const lpaIdCounter = lpaIdStore.load("counter") ?? 100;
+const body = JSON.parse(context.request.body);
 
-respond()
-  .withStatusCode(201)
-  .withContent(`{"uid":"${700000000000 + lpaIdCounter}"}`);
+if (body.batchId == 'bad-batch') {
+  respond()
+    .withStatusCode(400)
+    .withContent('{}');
+} else {
+  const lpaIdCounter = lpaIdStore.load("counter") ?? 100;
 
-lpaIdStore.save("counter", lpaIdCounter + 1);
+  respond()
+    .withStatusCode(201)
+    .withContent(`{"uid":"${700000000000 + lpaIdCounter}"}`);
+
+  lpaIdStore.save("counter", lpaIdCounter + 1);
+}
